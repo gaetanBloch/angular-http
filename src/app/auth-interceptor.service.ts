cdff009b03b2e6ecaca7c76f64c45a073957f8e8
Gaëtan Bloch <gaetan.bloch@gmail.com>
2020-04-16 23:26:10 +0200

    Manipulate the response in the Interceptor

--- src/app/auth-interceptor.service.ts
+++ src/app/auth-interceptor.service.ts
@@ -1,5 +1,12 @@
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import {
+  HttpEvent,
+  HttpEventType,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest
+} from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 export class AuthInterceptorService implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
@@ -9,6 +16,12 @@ export class AuthInterceptorService implements HttpInterceptor {
       {
         headers: req.headers.append('Auth', 'xyz')
       });
-    return next.handle(modifiedRequest);
+    return next.handle(modifiedRequest).pipe(tap(event => {
+      console.log(event);
+      if (event.type === HttpEventType.Response) {
+        console.log('Response arrived, body data: ');
+        console.log(event.body);
+      }
+    }));
   }
 }
