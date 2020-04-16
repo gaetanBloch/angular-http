fb0130161ed9b3cc19f6bcb42e943aa9ac123530
Gaëtan Bloch <gaetan.bloch@gmail.com>
2020-04-16 23:19:30 +0200

    Added Headers data with Interceptor

--- src/app/auth-interceptor.service.ts
+++ src/app/auth-interceptor.service.ts
@@ -4,6 +4,11 @@ import { Observable } from 'rxjs';
 export class AuthInterceptorService implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     console.log('Request is on its ways');
-    return next.handle(req);
+    console.log(req.url);
+    const modifiedRequest = req.clone(
+      {
+        headers: req.headers.append('Auth', 'xyz')
+      });
+    return next.handle(modifiedRequest);
   }
 }
